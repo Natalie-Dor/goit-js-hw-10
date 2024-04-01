fc528c0b8a88836fcfbbe9841883f4712bb62e48
Natalie-Dor <varnata000@gmail.com>
2024-04-01 13:20:56 +0300

    update snackbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -6,37 +6,41 @@ import 'izitoast/dist/css/iziToast.min.css';
 // `✅ Fulfilled promise in ${delay}ms`;
 // `❌ Rejected promise in ${delay}ms`;
 
-const createBtn = document.querySelector('button');
-const delay = document.querySelector('input[name="delay"]');
+// ===================================================================
+const form = document.querySelector('.form');
+// let delay = event.currentTarget.elements.delay.value;
+// let state = event.currentTarget.elements.state.value;
 
-createBtn.addEventListener('click', createNotification);
+form.addEventListener('submit', event => {
+  let delay = event.currentTarget.elements.delay.value;
+  let state = event.currentTarget.elements.state.value;
 
-function createNotification(e) {
-  e.preventDefault();
-  let state = document.querySelector('input[name="state"]:checked');
-  if (delay.value && state.value) {
-    const promise = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (state.value == 'fulfilled') {
-          resolve();
-          console.log(promise);
-          iziToast.success({
-            title: 'Success!',
-            message: `✅ Fulfilled promise in ${delay.value}ms`,
-            position: 'topRight',
-          });
-        }
-        if (state.value == 'rejected') {
-          reject(`Rejected promise in ${delay.value}ms`);
-          console.log(promise);
-          iziToast.error({
-            title: 'Error',
-            message: `❌ Rejected promise in ${delay.value}ms`,
-            position: 'topRight',
-          });
-        }
-      }, delay.value);
+  event.preventDefault();
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve();
+      } else {
+        reject();
+      }
+      console.log(delay, state);
+    }, delay);
+  });
+
+  promise
+    .then(value => {
+      iziToast.success({
+        color: 'green',
+        position: 'topRight',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch(error => {
+      iziToast.error({
+        color: 'red',
+        position: 'topRight',
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
     });
-    return promise;
-  }
-}
+});
